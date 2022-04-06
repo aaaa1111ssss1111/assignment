b65d76449a072ad22b60ae10a21512658482d5e8
aaaa1111ssss1111 <102968261+aaaa1111ssss1111@users.noreply.github.com>
2022-04-06 14:07:16 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,23 @@
 
+#!/usr/bin/env bash
+
+function guess(){
+    answer=$(ls -l |grep "^-"|wc -l)
+    while true;
+    do
+        echo "please guess the number"
+        read  number
+        if [ $number -lt $answer ]
+        then
+            echo "your guess is lesser"
+        elif [ $number -gt $answer ]
+        then
+            echo "your guess is greater"
+        else
+            echo "Correct guess"
+        break;
+        fi
+    done
+}
+echo "guess the file number"
+guess
